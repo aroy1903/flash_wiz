40fc597ed6e7c9f91d5c47dc5716a75a240f12a7
aroy1903 <ananthroy1903@gmail.com>
2024-01-27 19:35:55 -0800

    server

--- app/firebase/config.js
+++ app/firebase/config.js
@@ -1,9 +1,8 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+import { initializeApp } from "firebase/app";
+
+
+
 const firebaseConfig = {
   apiKey: "AIzaSyAOL-VJ1jAUGG2CGyBci6rI0J4MTpD8CHk",
   authDomain: "flashwiz-9e224.firebaseapp.com",
@@ -13,7 +12,7 @@ const firebaseConfig = {
   appId: "1:449039789177:web:796e21a9108ead0386b8a8"
 };
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
 
 export default app
